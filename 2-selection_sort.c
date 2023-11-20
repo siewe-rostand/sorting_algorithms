7a3c31d1a131c8445bdd6d63008be0dbe2e6ac53
siewe-rostand <rostand.siewe9@gmail.com>
2023-11-20 14:38:34 -0800

    2. Selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+
+/**
+ * selection_sort - selection sort algorithm use to sort
+ * an array of integers in ascending order
+ *
+ * @array: input array of integers
+ * @size: size of the array
+ * Return: no return
+ */
+void selection_sort(int *array, size_t size)
+{
+	int tmp;
+	size_t i, j, k;
+
+	for (i = 0; i < size; i++)
+	{
+		m = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[k] > array[j])
+				k = j;
+		}
+
+		if (i != m)
+		{
+			tmp = array[i];
+			array[i] = array[k];
+			array[k] = tmp;
+			print_array(array, size);
+		}
+	}
+}
